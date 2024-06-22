9a32f8d181b89cdb767a3fee93602ccd19ab37f4
Nathan King <nathanking3590@gmail.com>
2024-06-22 22:52:09 +0100

    add initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import tsIcon from './assets/icons/ts.png'
 import './App.css'
+import DirectorySVG from './components/svg/DirectorySVG'
 
-function App() {
-  return <p className='text-3xl font-bold text-red-500'>Hello, world!</p>
+export default function App() {
+  return (
+    <div className="drawer h-full lg:drawer-open">
+      <input id="drawer-toggle" type="checkbox" className="drawer-toggle" />
+
+      <main className="drawer-content flex flex-col items-center justify-center">
+        <label htmlFor="drawer-toggle" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+        <p>Main</p>
+      </main>
+
+      <aside className="drawer-side">
+        <ul className="menu menu-xs bg-base-200 rounded-lg max-w-xs w-60 h-full">
+          <li><label htmlFor="drawer-toggle" aria-label="close sidebar" className="drawer-overlay lg:hidden">Close</label></li>
+
+          <li>
+            <details open>
+              <summary>
+                <DirectorySVG /> src
+              </summary>
+
+              <ul>
+                <li className='explorer-item'>
+                  <a>
+                    <img src={tsIcon} />experience.ts
+                  </a>
+                </li>
+                <li className='explorer-item'>
+                  <a>
+                    <img src={tsIcon} />experience.spec.ts
+                  </a>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      
+      </aside>
+    </div>
+  )
 }
-
-export default App
