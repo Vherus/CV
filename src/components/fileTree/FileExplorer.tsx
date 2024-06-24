656e0b0636e5b559f6bca50933f578603ff526f1
Nathan King <nathanking3590@gmail.com>
2024-06-24 11:34:34 +0100

    use api

--- src/components/fileTree/FileExplorer.tsx
+++ src/components/fileTree/FileExplorer.tsx
@@ -4,7 +4,7 @@ import './styles/fileExplorer.css'
 export default function FileExplorer({ children }: PropsWithChildren) {
   return (
     <ul className="menu menu-md fe-bg-dark max-w-xs w-60 h-full p-3">
-      {(children as ReactNode[]).map(child => <li>{child}</li>)}
+      {(children as ReactNode[]).map((child, i) => <li key={i}>{child}</li>)}
     </ul>
   )
 }
--- src/pages/Experience.page.tsx
+++ src/pages/Experience.page.tsx
@@ -1,12 +1,17 @@
-import { getExperience } from "../api/apiClient"
+import { Experience, getExperience } from "../api/apiClient"
 import ExperienceBlock from "../components/experience/ExperienceBlock"
+import { useEffect, useState } from "react"
 
 export default function ExperiencePage() {
-  const experience = getExperience()
+  const [experience, setExperience] = useState<Experience[]>([])
+
+  useEffect(() => {
+    getExperience().then(setExperience)
+  }, [])
 
   return (
     <section>
-      {experience.map((exp, index) => <ExperienceBlock experience={exp} showBreak={index > 0} />)}
+      {experience.map((exp, index) => <ExperienceBlock key={exp.id} experience={exp} showBreak={index > 0} />)}
     </section>
   )
 }
