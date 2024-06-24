71bbfba2b0e13751f4837cfd3d23cdf7ac876fe1
Nathan King <nathanking3590@gmail.com>
2024-06-24 11:54:04 +0100

    add about content

--- src/pages/About.page.tsx
+++ src/pages/About.page.tsx
@@ -1,3 +1,12 @@
 export default function AboutPage() {
-  return <p>About</p>
+  return (
+    <article>
+      <p className="mb-3">This app is a WIP but please feel free to browse the github repositories:</p>
+      <ul className="mb-3">
+        <li><a className="color-keyword" href="" target="_blank">Client (React, Typescript, Tailwind)</a></li>
+        <li><a className="color-keyword" href="" target="_blank">Server (Express, Typescript)</a></li>
+      </ul>
+      <p>Please note that these are incomplete. I will be adding polish, tests, Context, a Postgres DB and perhaps an ORM like Prisma.</p>
+    </article>
+  )
 }
