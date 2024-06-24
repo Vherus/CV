4492cc26ad28727468b7fef9042095525848e125
Nathan King <nathanking3590@gmail.com>
2024-06-24 11:54:23 +0100

    add github links

--- src/pages/About.page.tsx
+++ src/pages/About.page.tsx
@@ -3,8 +3,8 @@ export default function AboutPage() {
     <article>
       <p className="mb-3">This app is a WIP but please feel free to browse the github repositories:</p>
       <ul className="mb-3">
-        <li><a className="color-keyword" href="" target="_blank">Client (React, Typescript, Tailwind)</a></li>
-        <li><a className="color-keyword" href="" target="_blank">Server (Express, Typescript)</a></li>
+        <li><a className="color-keyword" href="https://github.com/vherus/cv" target="_blank">Client (React, Typescript, Tailwind)</a></li>
+        <li><a className="color-keyword" href="https://github.com/vherus/cv-server" target="_blank">Server (Express, Typescript)</a></li>
       </ul>
       <p>Please note that these are incomplete. I will be adding polish, tests, Context, a Postgres DB and perhaps an ORM like Prisma.</p>
     </article>
