9a21d0f93a6651a56f3925f1ee6f3a07a84099ed
Nathan King <nathanking3590@gmail.com>
2024-06-24 22:29:27 +0100

    update experience page

--- src/pages/Experience.page.tsx
+++ src/pages/Experience.page.tsx
@@ -1,3 +1,4 @@
+import { Link } from "wouter"
 import ExperienceBlock from "../components/experience/ExperienceBlock"
 import { useData } from "../context/DataContext"
 
@@ -7,6 +8,7 @@ export default function ExperiencePage() {
   return (
     <section>
       {isLoading && <p>Gaining experience...</p>}
+      {!isLoading && <p className="mb-3">This app is a work in progress. View the <Link href="/about">about</Link> page for repository links.</p>}
       {experience.map((exp, index) => <ExperienceBlock key={exp.id} experience={exp} showBreak={index > 0} />)}
     </section>
   )
