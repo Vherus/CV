59fb47ec92675a81005906f64074d082af91e97c
Nathan King <nathanking3590@gmail.com>
2024-07-08 11:41:46 +0100

    update about

--- src/pages/About.page.tsx
+++ src/pages/About.page.tsx
@@ -1,14 +1,16 @@
 export default function AboutPage() {
   return (
     <section>
-      <p className="mb-3">This project is a WIP but please feel free to browse the github repositories:</p>
-      <ul className="mb-3">
-        <li className="mb-3">
-          <a className="color-keyword" href="https://github.com/vherus/cv-server" target="_blank">Server (Express, Typescript)</a>
-          <p><em>object oriented, repository pattern, zod for request validation, jest suite</em></p>
-        </li>
-        <li><a className="color-keyword" href="https://github.com/vherus/cv" target="_blank">Client (React, Typescript, Tailwind)</a></li>
-      </ul>
+      <article className="p-4 pt-2">
+        <p className="mb-3">This project is a WIP but please feel free to browse the github repositories:</p>
+        <ul className="mb-3">
+          <li className="mb-3">
+            <a className="color-keyword" href="https://github.com/vherus/cv-server" target="_blank">Server (Express, Typescript)</a>
+            <p><em>object oriented, repository pattern, zod for request validation, jest suite</em></p>
+          </li>
+          <li><a className="color-keyword" href="https://github.com/vherus/cv" target="_blank">Client (React, Typescript, Tailwind)</a></li>
+        </ul>
+      </article>
     </section>
   )
 }
